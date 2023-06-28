8e2cdfe4811ba77c3d1f4aef837f5c3556b32afb
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-06-28 16:33:35 -0500

    Add event to display/hide mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const speakersDiv = document.querySelector('.speakers');
 const footerSection = document.querySelector('.footer');
 const partnerSection = document.querySelector('.partner');
+// Mobile Menu //
+const menuButton = document.querySelector('.navBtnMenu');
+const mobileMenu = document.querySelector('.nav__mobile_menu');
+const navMenu = document.querySelector('.header_bar');
+const mobileMenuOptions = document.querySelectorAll('.nav_btn_list');
+const navBtnMobileClosed = document.querySelector('.nav__btn_mobile_closed');
+const navMainMenuMob = document.querySelector('.nav_main_mobileMenu');
 // speakers data //
 const speakersData = [{
   photo: 'images/ponente1.png', name: 'Teresa Núquez', position: 'Jueza Constitucional del Ecuador', backg: 'Caso Estrellita',
@@ -125,3 +132,20 @@ speakerBtnMob.addEventListener('click', () => {
 speakersDiv.appendChild(speakerBtnMob);
 speakerBtnMob.appendChild(speakerBtnMore);
 speakerBtnMob.appendChild(speakerBtnArrow);
+menuButton.addEventListener('click', () => {
+  mobileMenu.style.display = 'flex';
+  navMainMenuMob.style.display = 'block';
+  navMenu.style.display = 'none';
+});
+for (let i = 0; i < mobileMenuOptions.length; i += 1) {
+  mobileMenuOptions[i].addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+    navMainMenuMob.style.display = 'none';
+    navMenu.style.display = 'block';
+  });
+}
+navBtnMobileClosed.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+  navMainMenuMob.style.display = 'none';
+  navMenu.style.display = 'block';
+});
