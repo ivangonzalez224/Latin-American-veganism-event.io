ca889fa2baaee3acc232c5bef10b836882539a2d
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-06-29 13:59:11 -0500

    Add styles to the previous events section

--- about.js
+++ about.js
@@ -7,19 +7,19 @@ const navBtnMobileClosed = document.querySelector('.nav__btn_mobile_closed');
 const navMainMenuMob = document.querySelector('.nav_main_mobileMenu');
 // speakers data //
 menuButton.addEventListener('click', () => {
-    mobileMenu.style.display = 'flex';
-    navMainMenuMob.style.display = 'block';
-    navMenu.style.display = 'none';
-  });
-  for (let i = 0; i < mobileMenuOptions.length; i += 1) {
-    mobileMenuOptions[i].addEventListener('click', () => {
-      mobileMenu.style.display = 'none';
-      navMainMenuMob.style.display = 'none';
-      navMenu.style.display = 'block';
-    });
-  }
-  navBtnMobileClosed.addEventListener('click', () => {
+  mobileMenu.style.display = 'flex';
+  navMainMenuMob.style.display = 'block';
+  navMenu.style.display = 'none';
+});
+for (let i = 0; i < mobileMenuOptions.length; i += 1) {
+  mobileMenuOptions[i].addEventListener('click', () => {
     mobileMenu.style.display = 'none';
     navMainMenuMob.style.display = 'none';
     navMenu.style.display = 'block';
   });
+}
+navBtnMobileClosed.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+  navMainMenuMob.style.display = 'none';
+  navMenu.style.display = 'block';
+});
